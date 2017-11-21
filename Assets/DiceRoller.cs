021013ae3c48237af3bf1c4cb7750cbf6ac6490c
Lorneytunes <lorneytunez@gmail.com>
2017-11-21 20:00:10 +1100

    Diceroller added

---
+++ Assets/DiceRoller.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DiceRoller : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        RollResult = new int();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public int RollResult;
+
+    public Sprite[] DiceImages;
+
+    public void RollDie()
+    {
+        RollResult = Random.Range(1,7);
+        Debug.Log("Rolled: " + RollResult);
+        this.transform.GetComponent<Image>().sprite = DiceImages[RollResult - 1];
+    }
+
+}
